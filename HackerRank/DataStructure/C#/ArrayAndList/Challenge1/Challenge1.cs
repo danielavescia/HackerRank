2c229ba93fbc4804bd3082d5ab1af10ef60546f8
Daniela Vescia <danivescia@gmail.com>
2024-01-03 10:14:17 -0300

    Primeiro desafio

---
+++ HackerRank/DataStructure/C#/ArrayAndList/Challenge1/Challenge1.cs
@@ -0,0 +1,66 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace HackerRank.DataStructures.C_.ArrayAndList.Challenge1
+{
+    class Challenge1
+    {
+        public static int birthdayCakeCandles( List<int> candles )
+        {
+            int biggestCandles = 0;
+            int totalBiggestCandles = 0;
+
+
+            foreach ( int candle in candles )
+            {
+                bool constraint = IsCandleOkay( candle );
+
+                if ( constraint == true )
+                {
+                    if ( candle > biggestCandles )
+                    {
+                        biggestCandles = candle;
+                    }
+                }
+            }
+
+            foreach ( int candle in candles )
+            {
+                if ( candle >= biggestCandles )
+                {
+                    totalBiggestCandles++;
+                }
+            }
+
+
+            return totalBiggestCandles;
+
+        }
+        public static bool IsCandleOkay( int candle )
+        {
+            bool isOkay = false;
+
+
+            if ( candle >= 1 && candle <= Math.Pow( 10, 7 ) )
+            {
+                isOkay = true;
+            }
+
+            return isOkay;
+        }
+    }
+}
+
+
+
---
+++ HackerRank/DataStructure/C#/ArrayAndList/Challenge1/Challenge1Tests.cs
@@ -0,0 +1,52 @@
+﻿
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+namespace HackerRank.DataStructures.C_.ArrayAndList.Challenge1
+{
+    [TestFixture]
+    public class Challenge1Tests
+    {
+        [Test]
+        public void TestBirthdayCakeCandles()
+        {
+            List<int> candles = new() { 1, 4, 2, 3 };
+            int result = Challenge1.birthdayCakeCandles( candles );
+            ClassicAssert.AreEqual( 1, result );
+        }
+
+        [Test]
+        public void TestBirthdayCakeCandles2()
+        {
+            List<int> candles = new() { 1, 4, 2, 3, 4, 3, 4 };
+            int result = Challenge1.birthdayCakeCandles( candles );
+            ClassicAssert.AreEqual( 3, result );
+        }
+
+        [Test]
+        public void TestBirthdayCakeCandles_WithZeroCandles()
+        {
+            List<int> candles = new() { };
+            int result = Challenge1.birthdayCakeCandles( candles );
+            ClassicAssert.AreEqual( 0, result );
+        }
+
+        [Test]
+        public void TestIsCandleOkay_WithValidCandle()
+        {
+            int candle = 5;
+            bool result = Challenge1.IsCandleOkay( candle );
+            ClassicAssert.IsTrue( result );
+        }
+
+        [Test]
+        public void TestIsCandleOkay_WithInvalidCandle()
+        {
+            int candle = 15;
+            bool result = Challenge1.IsCandleOkay( candle );
+            ClassicAssert.IsFalse( result );
+        }
+    }
+
+}
+
